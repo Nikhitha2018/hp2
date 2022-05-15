c6f5069f1d5eaf5948847d1ab41c5b8992022670
Nikhitha2018 <62793320+Nikhitha2018@users.noreply.github.com>
2022-05-15 19:35:51 +0530

    Update settings.py

--- hackathon/settings.py
+++ hackathon/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'l=oc1ljo5a6j7h%j9s+^fvk)l94-2uyl8^ioj(#-(+=@n9%c-2'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','phishing-app-classification123.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1','phish-url-classification123.herokuapp.com']
 
 
 # Application definition
